771b3bba46f1eeb276deb4c3f5fe19b2e82dfe7d
Gabriel Nicutari <gabriel_nicutari@yahoo.ro>
2020-10-29 19:34:12 +0100

    Search (by keyword in multiple columns) + Delete

--- src/main/java/com/example/demo/Controller/GameController.java
+++ src/main/java/com/example/demo/Controller/GameController.java
@@ -3,12 +3,12 @@ package com.example.demo.Controller;
 import com.example.demo.Model.Game;
 import com.example.demo.Repository.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -27,6 +27,27 @@ public class GameController {
         }
 
         return new ResponseEntity<>(games, HttpStatus.OK);
+    }
+
+    //inside fetch
+    @GetMapping("/s")
+    public ResponseEntity<List<Game>> fetchAll2(@RequestParam(required = false) String key) {
+        List<Game> games = new ArrayList<>();
+
+        if(key == null) {
+            games.addAll(gameRepository.findAll());
+        } else {
+            games.addAll(gameRepository.findAllByKeyword(key));
+        }
+
+        return new ResponseEntity<>(games, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<HttpStatus> deleteTutorial(@PathVariable("id") int id) {
+        gameRepository.deleteById(id);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
